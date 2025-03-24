8f8af76f9042d9612a48741755df7b2221b77c22
ahvvad <ahvadd.k@gmail.com>
2025-03-25 01:13:15 +0200

    Add Transaction History
    - add transaction history header.
    - add transaction history item.
    - add transaction history model.
    - linked the model with the item after repeat it three times witht list view.
    - grouping MyCard section and TransactionHistory section in one section.
    - Finshing the MyCard & TransactionHistory Feature.

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,8 +1,8 @@
-import 'package:dashboard/widgets/my_cards_section.dart';
 import 'package:flutter/material.dart';
 
 import 'package:dashboard/widgets/all_expensess_and_quick_invoice_section.dart';
 import 'package:dashboard/widgets/custom_drawer.dart';
+import 'package:dashboard/widgets/my_cards_and_transction_history_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -15,7 +15,7 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(width: 24),
         Expanded(flex: 2, child: AllExpensessAndQuickInvoiceSection()),
         SizedBox(width: 24),
-        Expanded(child: MyCardsSection()),
+        Expanded(child: MyCardsAndTransctionHistorySection()),
       ],
     );
   }
